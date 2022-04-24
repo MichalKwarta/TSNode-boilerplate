d86e1f08aacd1340dd8f006a1f00ec3e006b8ec2
MichalKwarta <Michu1164@gmail.com>
2022-04-24 18:08:05 +0200

    init

---
+++ routes/index.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response,Router } from 'express'
+const router = Router();
+
+/* GET home page. */
+router.get('/', function(req:Request, res:Response, next:NextFunction):void {
+  res.render('index', { title: 'Express' });
+});
+
+export { router };
